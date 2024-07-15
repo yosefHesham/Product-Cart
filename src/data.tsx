0d482f9dff1591617b94a8dc912c4da1c5f2389f
yosefHesham <yousefmobdev@gmail.com>
2024-07-15 16:34:32 +0300

    Providing some clarificaition

--- src/data.tsx
+++ src/data.tsx
@@ -1,6 +1,7 @@
-// (assuming that we have variety of products), i will create object that has name, brand and color and price
-// this might lead for redundancy (repeating name, brand & price for each shirt) but its intended :)
-// if its just about one product, i would just identify each shirt with a color, with constant name and decription and price.
+// at first glance i thought of creating list of products, but after giving attention, its only one product
+// that product has list of colors and sizes
+// my imagination is that this can be product details page.
+// in a complete website the flow will be (ProductsPage that renders list of products) => click on a product to go to product details
 export const colors = ["blue", "black", "orange", "red", "white", "purple"];
 
 export const product = {
@@ -10,45 +11,6 @@ export const product = {
   sizes: ["Small", "Medium", "Large", "Extra Large", "XXL"],
   price: 71.56,
 };
-
-// export const products = [
-//   {
-//     name: "Polo Shirt",
-//     brand: "Teixeira Design Studio",
-//     color: "black",
-//     price: 71.56,
-//   },
-//   {
-//     name: "Polo Shirt",
-//     brand: "Teixeira Design Studio",
-//     color: "blue",
-//     price: 71.56,
-//   },
-//   {
-//     name: "Polo Shirt",
-//     brand: "Teixeira Design Studio",
-//     color: "orange",
-//     price: 71.56,
-//   },
-//   {
-//     name: "Polo Shirt",
-//     brand: "Teixeira Design Studio",
-//     color: "red",
-//     price: 71.56,
-//   },
-//   {
-//     name: "Polo Shirt",
-//     brand: "Teixeira Design Studio",
-//     color: "white",
-//     price: 71.56,
-//   },
-//   {
-//     name: "Polo Shirt",
-//     brand: "Teixeira Design Studio",
-//     color: "purple",
-//     price: 71.56,
-//   },
-// ];
 
 interface SizeMap {
   [key: string]: number;
