f2f51e1f2a24dfca0c70ba0fb837c6ff94f2c36f
yosefHesham <yousefmobdev@gmail.com>
2024-07-15 14:38:15 +0300

    Fix Build Error

--- src/components/ProductsGallery.tsx
+++ src/components/ProductsGallery.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 const ProductsGallery = ({
   images,
   onSelect,
